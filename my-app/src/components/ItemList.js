af975f17f2f3f1a6430fa089e3b39abc74732dc2
VictoriaVigroux <vigrouxv@gmail.com>
2022-07-29 19:10:57 -0300

    ItemList

---
+++ my-app/src/components/ItemList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Producto from "../Item/Item";
+import { Contenedorproductos } from "./styles";
+
+const ItemList = ({ products }) => {
+  return (
+    <Contenedorproductos>
+      {products?.length > 0 &&
+        products?.map((item) => (
+          <Producto
+            key={item.id}
+            title={item.title}
+            price={item.price}
+            images={item.images}
+            id={item.id}
+          />
+        ))}
+    </Contenedorproductos>
+  );
+};
+export default ItemList;
