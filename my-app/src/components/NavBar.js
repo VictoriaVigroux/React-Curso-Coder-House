eee21c8585f898ef3a5396a83170ffbcbcc5be82
VictoriaVigroux <vigrouxv@gmail.com>
2022-07-29 18:37:24 -0300

    Categorias clickleables

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -2,4 +2,29 @@ const NavBar = () => {
   return <div>Tienda Online</div>;
 };
 
+return (
+  <Container>
+    <Logo src="" />
+    <Navegacion>
+      <ItemsNavegacion>
+        <Links to="/">Inicio</Links>
+      </ItemsNavegacion>
+      <ItemsNavegacion>
+        <Links to="/">Productos</Links>
+      </ItemsNavegacion>
+      <ItemsNavegacion>
+        <Links to="/">Contacto</Links>
+      </ItemsNavegacion>
+      <ItemsNavegacion>
+        <Links to="/">Comprar</Links>
+      </ItemsNavegacion>
+    </Navegacion>
+    <RightSide>
+      <p>Email</p>
+      <CartWidget state={state} setMenu={setMenu} menu={menu} />
+      {menu && <MenuCarrito />}
+    </RightSide>
+  </Container>
+);
+
 export default NavBar;
